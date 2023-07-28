e73d24ba1277f57a72b1b3b529143715c76e5466
mahmoud842 <mahmoud1hesham2003@gmail.com>
2023-07-28 19:42:02 +0300

    fix a bug in winner function

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function winner(board) {
     }
 
     // check sec diagonal
-    if (board[2][0] == board[1][1] && board[1][1] == [0][2] && board[1][1] != EMPTY) {
+    if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[1][1] != EMPTY) {
         return board[1][1];
     }
 
