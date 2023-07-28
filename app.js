a881309b969133b1f6c582a0c17b2e36d1989868
mahmoud842 <mahmoud1hesham2003@gmail.com>
2023-07-28 13:00:16 +0300

    add game setup function - store cells objects in an

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-function player() { }
-function terminal() { }
-function winner() { }
-function actions() { }
-function result() { }
-function utility() { }
-function minimax() { }
+EMPTY = null;
+X = 'X';
+O = 'O';
+
+function intialState(board) {
+    board.push([EMPTY, EMPTY, EMPTY]);
+    board.push([EMPTY, EMPTY, EMPTY]);
+    board.push([EMPTY, EMPTY, EMPTY]);
+}
+
+function player() {
+    tmp = gameOptions.playerCharacter;
+    if (tmp == X) {
+        gameOptions.playerCharacter = O;
+    }
+    else {
+        gameOptions.playerCharacter = X;
+    }
+    return tmp
+}
+
+function terminal() {
+    console.log('terminal not implemented');
+}
+
+function winner() {
+    console.log('winner not implemented');
+}
+
+function actions() {
+    console.log('action not implemented');
+}
+
+function result() {
+    console.log('result not implemented');
+}
+
+function utility() {
+    console.log('utililty not implemented');
+}
+
+function minimax() {
+    console.log('minimax not implemented');
+}
--- vendor.js
+++ vendor.js
@@ -8,20 +8,88 @@ const boardPage = document.getElementById('board');
 const playerVsPlayerBtn = document.getElementById('palyer-vs-player');
 const playerVsComputerBtn = document.getElementById('palyer-vs-computer');
 
+// board
+const board = [];
+const boardObjs = [];
+
+// options
 const gameOptions = {
     AI: false,
-    playerCharacter: 'X'
+    AIChar: null,
+    AITurn: false,
+    playerCharacter: 'X',
+    waitingForUserInput: false
 };
+
+
+function boardQuery() {
+    // #board .board .board-cell
+    tmpArr = document.getElementsByClassName('board-cell');
+    for (let i = 0; i < 3; i++) {
+        row = [];
+        for (let j = 0; j < 3; j++) {
+            row.push(tmpArr[(i * 3) + j]);
+        }
+        boardObjs.push(row);
+    }
+}
+
+function display() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            tmp = board[i][j];
+            if (tmp == null) {
+                boardObjs[i][j].innerText = ' ';
+            }
+            else {
+                boardObjs[i][j].innerText = tmp;
+            }
+        }
+    }
+}
 
 function AISetup() {
     console.log('AI');
 }
 
-function gameLoop() {
+function gameSetup() {
+    // setup board:
+    // getting all the cells of the board
+    // change to board page
+    // add click event listener to the cells
+    // intialize and display empty board
+    boardQuery();
     homePage.style.display = 'none';
     boardPage.style.display = 'flex';
+    intialState(board);
+    addEvenetListenerToCells();
+    display();
+}
+
+function gameLoop() {
     console.log('reached game loop');
 }
 
-playerVsPlayerBtn.addEventListener('click', gameLoop);
+// click handler for each cell in the board
+function cellsEvent(action) {
+    if (gameOptions.waitingForUserInput) {
+        console.log(action);
+        gameLoop();
+    }
+    else {
+        console.log(`user action (${action[0]} , ${action[1]}) but ignored`);
+    }
+}
+
+// event listener to main page buttons
+playerVsPlayerBtn.addEventListener('click', gameSetup);
 playerVsComputerBtn.addEventListener('click', AISetup);
+
+// a funtion to add event listener to each cell in the board
+function addEvenetListenerToCells() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            boardObjs[i][j].addEventListener('click', cellsEvent.bind(this, [i, j]));
+        }
+    }
+}
